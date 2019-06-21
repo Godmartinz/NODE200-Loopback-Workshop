8b9cbdbb0bc6b0807714e57085f0d4a63a78dc65
Godfrey Martinez <godmartinz@gmail.com>
2019-06-21 15:01:15 -0700

    adds initial files

---
+++ server/datasources.production.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+    "MongoDB": {
+      "host": "",
+      "port": "",
+      "url": process.env.MONGODB_URI,
+      "database": "",
+      "password": "",
+      "name": "MongoDB",
+      "user": "",
+      "connector": "mongodb"
+    }
+  }
