e4b953b64a5e3fb060d3c8c82467ff4e892bf74a
Godfrey Martinez <godmartinz@gmail.com>
2019-06-21 15:21:11 -0700

    added circleCi

--- server/datasources.local.js
+++ server/datasources.local.js
@@ -7,7 +7,7 @@ module.exports = {
   MongoDB: {
     host: "",
     port: 0,
-    url: process.env.MONGODB_URI,
+    url: 'mongodb://test:tester1@ds139956.mlab.com:39956/heroku_53ft6v4n',
     database: "",
     password: "",
     name: "MongoDB",
